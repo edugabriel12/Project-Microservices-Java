9fda6349f69e9021ae0bfc28e001228e5a532f96
eduardo.cardoso <eduardo.cardoso.ext@synchro.com.br>
2023-01-02 15:38:47 -0300

    adding initial services to the project

---
+++ PhotoAppApiUsers/src/test/java/com/photopp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photopp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppDiscoveryService/src/test/java/com/course/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
