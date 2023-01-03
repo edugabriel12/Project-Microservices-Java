0c5dc79fd25770f047fb3b04c03c0783cff4759b
eduardo.cardoso <eduardo.cardoso.ext@synchro.com.br>
2023-01-03 15:42:19 -0300

    Allowing multiple instances of the same service to run in parallel

--- PhotoAppApiUsers/src/main/java/com/photopp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/photopp/api/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.photopp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+
+        return "Working on port: " + env.getProperty("local.server.port");
     }
 }
